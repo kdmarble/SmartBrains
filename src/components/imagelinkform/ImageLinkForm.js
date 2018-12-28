521895392c2338a2604ee130010c26452a25d1af
Keith Marble <kmarble96@gmail.com>
2018-12-28 12:50:34 -0500

    removed this in handlekeypress

--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit, handleKeyPress }) => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} onKeyPress={this.handleKeyPress} />
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} onKeyPress={handleKeyPress} />
                     <button id='detectButton' className='f4 w-30 grow link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit} onKeyPress={handleKeyPress}>Detect</button>
                 </div>
             </div>
